517de075cfd59da47c2c510e0ea445c89f9e3da2
qitas <qitas@qitas.cn>
2021-05-27 16:49:23 +0800

    fix

--- src/dapboot/src/usb_conf.c
+++ src/dapboot/src/usb_conf.c
@@ -1,20 +1,3 @@
-/*
- * Copyright (c) 2016, Devan Lai
- *
- * Permission to use, copy, modify, and/or distribute this software
- * for any purpose with or without fee is hereby granted, provided
- * that the above copyright notice and this permission notice
- * appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
- * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
- * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
- * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
- * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
- * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
 
 #include <stdlib.h>
 #include <stdint.h>
@@ -99,12 +82,12 @@ static char serial_number[USB_SERIAL_NUM_LENGTH+1];
 static const char *usb_strings[] = {
     "STM32 DFU",
 #ifdef BOOTLOADER_HIGH
-    "STM32 DAP DFU Bootloader",
+    "DAP DFU Bootloader(64K)",
 #else
-    "DAP DFU Bootloader",
+    " DAP DFU Bootloader(8K)",
 #endif
     serial_number,
-    "DAPBoot DFU"
+    "STM32 DAP DFU"
 };
 
 /* Buffer to be used for control requests. */
